9bc623228a9aba28141b67d1ced52d28c348f527
Kyle Temkin <ktemkin@binghamton.edu>
2012-07-22 20:28:41 -0400

    EVALMATH added array, logic, and string modules to the core

--- mathscript_string.php
+++ mathscript_string.php
@@ -15,4 +15,29 @@ class mathscript_string
 	{
 		return str_pad($input, $pad_length, $pad_with, STR_PAD_LEFT);
 	}
+
+    /**
+     * Returns '1' iff the given haystack starts with the needle.
+     */
+    public static function starts_with($self, $haystack, $needle)
+    {
+        $length = strlen($needle);
+        return (substr($haystack, 0, $length) === $needle) ? 1 : 0;
+    }
+
+    public static function ends_with($self, $haystack, $needle)
+    {
+        $length = strlen($needle);
+        
+        //assume all 
+        if ($length == 0) 
+            return true;
+        
+        //calculate the start position with respect to the end of the string
+        $start  = $length * -1; //negative
+
+        //and check to see if the ends of the string are equal
+        return (substr($haystack, $start) === $needle);
+    }
+
 }
